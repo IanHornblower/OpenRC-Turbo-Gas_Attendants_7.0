75b0d2c5ec5ed50df18241b260ac74f798948e2c
kittypickles9982 <admin@thatsfuckingstupid.org>
2021-11-26 16:53:38 -0500

    It  Works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Battery.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Battery.java
@@ -2,21 +2,41 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import org.firstinspires.ftc.teamcode.revextensions2.ExpansionHubEx;
 
+import java.util.Objects;
+
 public class Battery {
 
     public static ExpansionHubEx expansionHub;
+    public static int BatteryCapacity = 3000;
 
-
-    public static double percentage() {
-        // Returns 0 if Battery.voltage() returns ∞
-        double result = 0;
-        double v = expansionHub.read12vMonitor(ExpansionHubEx.VoltageUnits.VOLTS);
-        if(v != Double.POSITIVE_INFINITY) {
-            result = (v-10)*20;
-            if(result < 0)
-                result = 0;
-        }
-        return result;
+    public static int percentage() {
+        int result = 0;
+        double v = Math.round(voltage()/10)/100;
+        result = (int)(v-10)*20;
+        if(result < 0)
+            result = 0;
+        return Math.round(result);
     }
+
+    public static int timeRemaining(String timeUnit) {
+        float p = (float)percentage();
+        float a = (float)currentDraw();
+        double ret = Math.round((p/100)*60*(BatteryCapacity/a));
+        if (Objects.equals(timeUnit, "HOURS")) {
+            ret = Math.round(ret/60);
+        } else if (Objects.equals(timeUnit, "SECONDS")) {
+            ret = Math.round(ret*60);
+        }
+        return (int)Math.round(ret);
+    }
+
+    public static double currentDraw() {
+        return expansionHub.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS);
+    }
+
+    public static double voltage() {
+        return Math.round(expansionHub.read12vMonitor(ExpansionHubEx.VoltageUnits.MILLIVOLTS));
+    }
+
 
 }
