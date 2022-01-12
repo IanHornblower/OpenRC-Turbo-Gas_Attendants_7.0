0fc963078e82f607888897710c6b9e9c5cbcba92
Killian Townsend <oasisscript87@gmail.com>
2022-01-11 19:49:38 -0500

    Updated ControllerTest, Blinkin, and Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Blinkin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Blinkin.java
@@ -11,6 +11,10 @@ public class Blinkin {
 
     public static RevBlinkinLedDriver Driver = null;
 
+    private static long nt_old;
+    private static long nt_del;
+    private static long nt_now;
+
     public static void setColor(String cname) {
 
         if (Driver == null) throw new NullPointerException("Blinkin.Driver is Not Defined");
@@ -33,19 +37,17 @@ public class Blinkin {
 
     }
 
-    public static synchronized void updateLightTimer(int currentTime) {
-        updateLightTimer(currentTime, false);
-    }
-
     /**
      * The 'auto' boolean is optional, just set true for auto
      * @param currentTime time in lmao
-     * @param auto is it auto? not required lmao
      */
+    public static synchronized void updateLightTimer(int currentTime) {
+        updateLightTimer(currentTime, false);
+    }
     public static synchronized void updateLightTimer(int currentTime, boolean auto) {
         int matchLength = auto ? 30 : 150;
         currentTime = matchLength-(currentTime/1000);
-        Log.i("Blinkin T" + "Time", String.valueOf(currentTime));
+        Log.i("Blinkin.updateLightTimer()", "Count Down : "+currentTime);
         switch(currentTime) {
             case(35):
             case(32):
@@ -62,21 +64,42 @@ public class Blinkin {
             case(5):
             case(3):
             case(1):
-                Driver.setPattern(BlinkinPattern.WHITE);
+                Flash(BlinkinPattern.BLUE, BlinkinPattern.RED, 100);
                 break;
             case(4):
             case(2):
             case(0):
-                Driver.setPattern(BlinkinPattern.RED);
+                Flash(BlinkinPattern.HOT_PINK, BlinkinPattern.GREEN, 100);
                 break;
-
             default:
                 Driver.setPattern(BlinkinPattern.BLACK);
                 break;
         }
+    }
 
-
-
+    /**
+     * Flashes between two patterns
+     * @param P1 Blinkin Pattern 1
+     * @param P2 Blinkin Pattern 2
+     * @param duration Time of each flash in ms
+     */
+    private static void Flash(BlinkinPattern P1, BlinkinPattern P2, int duration) {
+        long total = 0;
+        nt_old = System.nanoTime();
+        boolean flip = false;
+        while(total < 1000) {
+            Log.i("Blinkin.Flash()", String.valueOf(total));
+            Log.i("Blinkin.Flash()", String.valueOf(nt_del % duration));
+            if (nt_del % duration != 0) {
+                nt_now = System.nanoTime();
+                nt_del = nt_now - nt_old;
+            } else {
+                Driver.setPattern(flip ? P1 : P2);
+                flip = !flip;
+            }
+            nt_old = System.nanoTime();
+            total = total + nt_del;
+        }
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Testing/ControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Testing/ControllerTest.java
@@ -32,8 +32,13 @@ public class ControllerTest extends LinearOpMode {
     public double time = 0;
     public double cycleLength = 0;
     public static double timeRunning = 0;
+<<<<<<< HEAD
     public static int BuildNumber = 235632752;
 >>>>>>> 214e7f6 (balls)
+=======
+    public static String BuildNumber = "2.4.7";
+    public static String BuildComment = "Updated Blinkin Timer";
+>>>>>>> c7e8e04 (bbw pt2)
     public static String HubName = "Expansion Hub 1";
     public static boolean ImperialUnits = false;
     public static String timeUnit = "HOURS";
