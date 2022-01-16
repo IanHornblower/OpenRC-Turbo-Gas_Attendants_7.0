6eef5a2e5bf5b498a9c13b86f684e910dc15b3f7
Killian Townsend <oasisscript87@gmail.com>
2022-01-16 14:23:49 -0500

    Updated battery class to factor in more than one Expansion Hub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Timer.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Timer {
+
+    protected Thread TimerThread;
+    Thre
+ad piss = new Thread(()-> {
+
+    });
+    piss.start();
+
+
+}
